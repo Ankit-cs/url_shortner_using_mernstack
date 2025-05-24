5ef4e207764d00a18211da6c80bdadfcdfcc5a1a
Ankit-cs <ankitcareer018@gmail.com>
2025-05-24 17:40:31 +0530

    just implemented custom error handler

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cabinate from "./src/config/db.config.js";
 import shortUrlRoutes from "./src/routes/shortUrl.route.js";
 import { redirectFromShort } from "./src/controller/shortUrl.controller.js";
+import { errorHandler } from "./src/utils/errorHandler.js";
 
 dotenv.config();
 const app = express();
@@ -10,7 +11,7 @@ const PORT = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(errorHandler)
 app.use("/api", shortUrlRoutes);
 app.get("/:id", redirectFromShort); 
 
