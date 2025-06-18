faa962eacdfc6832e1820188dc06c2b06cd65e5a
Ankit Kumar <ankitcareer018@gmail.com>
2025-06-18 17:52:09 +0530

    Update AuthPage.jsx
    init

--- FRONTEND/src/pages/AuthPage.jsx
+++ FRONTEND/src/pages/AuthPage.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import LoginForm from '../components/LoginForm'
 import RegisterForm from '../components/RegisterForm'
 import { useSearch } from '@tanstack/react-router'
 
 const AuthPage = () => {
-  const { mode } = useSearch({ from: '/auth' })
-  const isLogin = mode === 'login'
+  const search = useSearch({ from: '/auth' });
+  const [login, setLogin] = useState(true);
+
+  useEffect(() => {
+    if (search.form === 'signup') setLogin(false);
+    else if (search.form === 'login') setLogin(true);
+  }, [search.form]);
 
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center p-4">
-      {isLogin ? (
-        <LoginForm state={() => {}} />
-      ) : (
-        <RegisterForm state={() => {}} />
-      )}
+      {login ? <LoginForm state={setLogin} /> : <RegisterForm state={setLogin} />}
     </div>
   )
 }
