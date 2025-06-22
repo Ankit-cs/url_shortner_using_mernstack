da867acb3fe8f5c52506493e9e4d3f1600f0582b
Ankit-cs <ankitcareer018@gmail.com>
2025-06-22 11:19:14 +0530

    fix

--- FRONTEND/src/pages/DashboardPage.jsx
+++ FRONTEND/src/pages/DashboardPage.jsx
@@ -1,32 +1,33 @@
-import React from 'react'
-import UrlForm from '../components/UrlForm'
-import UserUrl from '../components/UserUrl'
-import { useNavigate } from '@tanstack/react-router'
+import React, { useEffect, useState } from 'react'
+import LoginForm from '../components/LoginForm'
+import RegisterForm from '../components/RegisterForm'
+import { useSearch, useNavigate } from '@tanstack/react-router'
+import { useSelector } from 'react-redux'
 
-const DashboardPage = () => {
-  const navigate = useNavigate();
+const AuthPage = () => {
+  const { mode } = useSearch({ strict: false }) // read ?mode=login or ?mode=register
+  const [login, setLogin] = useState(mode !== 'register')
+
+  const { isAuthenticated } = useSelector((state) => state.auth)
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (mode === 'register') setLogin(false)
+    else setLogin(true)
+  }, [mode])
+
+  // ✅ Redirect to dashboard if already logged in
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate({ to: '/dashboard' })
+    }
+  }, [isAuthenticated, navigate])
 
   return (
-    <div className="min-h-screen   bg-gray-100 flex flex-col items-center justify-center p-4">
-    <div className="bg-white -mt-20 p-8 rounded-lg shadow-md w-full max-w-4xl">
-      <div className="flex items-center justify-between mb-6">
-        <button
-          onClick={() => navigate({ to: '/' })}
-          className="text-gray-600 hover:text-gray-800 flex items-center"
-        >
-          <svg className="w-5 h-5 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
-          </svg>
-          Back
-        </button>
-        <h1 className="text-2xl font-bold">URL Shortener</h1>
-        <div className="w-12"></div> {/* Spacer for centering */}
-      </div>
-      <UrlForm/>
-      <UserUrl/>
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center p-4">
+      {login ? <LoginForm state={setLogin} /> : <RegisterForm state={setLogin} />}
     </div>
-  </div>
   )
 }
 
-export default DashboardPage
+export default AuthPage
